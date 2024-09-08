d7e618befe1ff9b426be41fb3b336a096f0f7820
CHIGODI <chiegody254@gmail.com>
2024-09-08 17:36:36 +0300

    Add uncessary files to be ignored- .gitignore Add environemnet requirements to requirements.txt Change app name to api Fix correction in main project to match app name (api)

--- BACK-END/LeafLife/api/urls.py
+++ BACK-END/LeafLife/api/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from django.urls import path
-from .views import UserListCreate, UserDetail
+from .views.users import UserListCreate, UserDetail
 
 urlpatterns = [
     path('users/', UserListCreate.as_view(), name='user-list-create'),
--- BACK-END/LeafLife/api/views/users.py
+++ BACK-END/LeafLife/api/views/users.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+#!/usr/bin/env python3
+""" This module contains users endpoints using Django Rest Framework CBV """
 from rest_framework import generics
 from ..models import User
 from ..serializers import UserSerializer
 
+
 class UserListCreate(generics.ListCreateAPIView):
+    """
+    Create a new user or list all users
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Update or delete a user
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'id'
