eb4376563c28e4361501da0f7fd6063287ef0178
DianaWangui <dianawanguikt048@gmail.com>
2024-09-08 18:06:11 +0300

    Added the dashboard login and signup pages Added axios to connect to the backend API to fetch data from and to the backend

--- FRONT-END/LeafLife/src/components/Navbar.jsx
+++ FRONT-END/LeafLife/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
@@ -8,30 +9,29 @@ const Navbar = () => {
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             {/* Logo */}
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <Link className="flex flex-shrink-0 items-center mr-4" to="/">
               <img className="h-20 w-auto" src={logo} alt="Logo" />
-              {/* <span className="hidden md:block text-green-500 text-2xl font-bold ml-2">Leaf Life</span> */}
-            </a>
-            <div className="md:ml-auto">
+            </Link>
+            <div className="md:ml-auto mt-4">
               <div className="flex space-x-2">
-                <a
-                  href="#"
+                <Link
+                  to="/"
                   className="text-green-500 bg-transparent hover:bg-green-400 hover:text-white rounded-md px-3 py-2 transition duration-300"
                 >
                   Home
-                </a>
-                <a
-                  href="#"
+                </Link>
+                <Link
+                  to="/contact"
                   className="text-green-500 bg-transparent hover:bg-green-400 hover:text-white rounded-md px-3 py-2 transition duration-300"
                 >
                   Contact
-                </a>
-                <a
-                  href="#"
+                </Link>
+                <Link
+                  to="/signup"
                   className="text-green-500 bg-transparent hover:bg-green-400 hover:text-white rounded-md px-3 py-2 transition duration-300"
                 >
                   Sign Up
-                </a>
+                </Link>
               </div>
             </div>
           </div>
