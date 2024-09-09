7ec3ea1897f0903f7cba5733eae156468f1ec961
CHIGODI <chiegody254@gmail.com>
2024-09-09 09:07:18 +0300

    Add serializers for all models

--- BACK-END/LeafLife/api/serializers.py
+++ BACK-END/LeafLife/api/serializers.py
@@ -1,7 +1,71 @@
+#!/usr/bin/env python3
+""" This module contains classes that serilize all models"""
+
 from rest_framework import serializers
-from .models import User  # Assuming you have a User model
+from .models import User
+from .models import Garden
+from .models import Bed
+from .models import CropRotation
+from .models import Crop
+from .models import Harvest
+from .models import Inspection
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password'] 
+        fields = ['id', 'username', 'email', 'password']
+
+
+class GardenSerializer(serializers.ModelSerializer):
+    """
+    Serialize garden class to json
+    """
+    class Meta:
+        model = Garden
+        fields = ['id', 'created_at', 'updated_at', 'name',
+                'longitude', 'latitude', 'description', 'user_id']
+
+
+class BedSerializer(serializers.ModelSerializer):
+    """
+    Serialize bed class to json
+    """
+    class Meta:
+        model = Bed
+        fields = ['id', 'created_at', 'updated_at', 'name',
+                  'garden_id']
+
+
+class CropRotation(serializers.ModelSerializers):
+    """
+    Serialize crop rotation class to json
+    """
+    class Meta:
+        model = CropRotation
+        fields = ['id', 'created_at', 'updated_at', 'garden',
+                  'previous_crop', 'next_crop']
+
+
+class Crop(serializers.ModelSerializers):
+    """Serialize crop class to json"""
+    class Meta:
+        model = Crop
+        fields = ['id', 'created_at', 'updated_at', 'name',
+                  'variety', 'planting_date', 'bed_id']
+
+
+class Harvest(serializers.ModelSerializers):
+    """Serialize harvest class to json"""
+    class Meta:
+        model = Harvest
+        fields = ['id', 'created_at', 'updated_at', 'bed_id',
+                  'crop_id', 'quantity_harvested', 'notes']
+
+
+class Inspection(serializers.ModelSerializers):
+    """Serialize inspection class to json"""
+    class Meta:
+        model = Inspection
+        fields = ['id', 'created_at', 'updated_at', 'user_id',
+                  'bed_id', 'crop_id', 'inspection_date', 'notes']
