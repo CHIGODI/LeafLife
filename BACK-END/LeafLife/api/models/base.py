23f536335fa2dc6da858cec0100bed643c43f411
julioceaseless <julioceaseless@gmail.com>
2024-09-09 12:53:20 +0300

    Add base methods

--- BACK-END/LeafLife/api/models/base.py
+++ BACK-END/LeafLife/api/models/base.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime
 from django.db import models
 import uuid
 
@@ -12,5 +12,26 @@ class Base(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        """Makes the model abstract, so it is not created in the database"""
+        """Makes Base model abstract, so it is not created in the database"""
         abstract = True
+    
+    def to_dict(self):
+        """Serializes an object to a dictionary"""
+        obj_dict = {field.name: getattr(self, field.name) for field in self._meta.fields}
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def save(self, *args, **kwargs):
+        """Save the object and update the 'updated_at' field"""
+        self.updated_at = datetime.utcnow()
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        """Delete the current instance"""
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        """String representation of the object"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.to_dict}"
