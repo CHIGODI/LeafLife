43325de98d147a05602fbde7878ebc2f6365228f
CHIGODI <chiegody254@gmail.com>
2024-09-10 11:55:08 +0300

    Fix created_at attribute to auto_now

--- BACK-END/LeafLife/api/models/base.py
+++ BACK-END/LeafLife/api/models/base.py
@@ -8,13 +8,13 @@ class Base(models.Model):
     id = models.UUIDField(primary_key=True,
                           default=uuid.uuid4,
                           editable=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         """Makes Base model abstract, so it is not created in the database"""
         abstract = True
-    
+
     def to_dict(self):
         """Serializes an object to a dictionary"""
         obj_dict = {field.name: getattr(self, field.name) for field in self._meta.fields}
