b5c160e447fe890b089e5f23c22a5a2fc08b0d57
julioceaseless <julioceaseless@gmail.com>
2024-09-12 23:12:08 +0300

    Add more endpoint urls

--- BACK-END/LeafLife/api/urls.py
+++ BACK-END/LeafLife/api/urls.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 from django.urls import path
-from .views.users import UserListCreate, UserDetail
+from .views.users import UserList, UserCreate, UserDetail
 from .views.gardens import GardenListCreate, GardenDetail
 from .views.beds import BedListCreate,  BedDetail
+from .views.register import RegisterUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views.login import CustomLoginJWTView
 
 urlpatterns = [
-    path('users/', UserListCreate.as_view(), name='user-list-create'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', CustomLoginJWTView.as_view(), name='login'),
+    path('register/', RegisterUserView.as_view(), name='register'),
+    path('user/', UserCreate.as_view(), name='user-create'),
+    path('users/', UserList.as_view(), name='user-list'),
     path('users/<str:id>/', UserDetail.as_view(), name='user-detail'),
     path('users/<str:id>/gardens/', GardenListCreate.as_view(), name='gardens-list-create'),
     path('users/<str:id>/gardens/<str:garden_id>/', GardenDetail.as_view(), name='gardens-detail'),
@@ -14,5 +22,5 @@ urlpatterns = [
     path('beds/', BedListCreate.as_view(), name='bed-list-create'),
     # path('beds/<int:id>/', BedDetail.as_view(), name='bed-detail'),
     # path('beds/<int:id>/crops/', BedDetail.as_view(), name='bed-crops'),
-    
+    # URLS to obtain and refresh tokens
 ]
